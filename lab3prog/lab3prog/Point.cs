0d47d60f1e6c2abd5795292931c545221231a10c
9lpkuu <9lpkuu2002@gmail.com>
2022-01-11 22:28:02 +0300

    successfully!

---
+++ lab3prog/lab3prog/Point.cs
@@ -0,0 +1,36 @@
+﻿namespace lab3prog
+{
+    class Point
+    {
+        int xPosition;
+        int yPosition;
+        char symbol;
+
+        public void SetX(int x)
+        {
+            if (x >= 0)
+                xPosition = x;
+            else
+                throw new Exception("Значение x не может быть отрицательным");
+        }
+        public void SetY(int y)
+        {
+            if (y >= 0)
+                yPosition = y;
+            else
+                throw new Exception("Значение y не может быть отрицательным");
+        }
+        public void SetSym(char sym)
+        {
+            symbol = sym;
+        }
+        public void Draw(int x1, int y1, char sym1)
+        {
+            SetX(x1);
+            SetY(y1);
+            SetSym(sym1);
+            Console.SetCursorPosition(xPosition, yPosition);
+            Console.Write(symbol);
+        }
+    }
+}
---
+++ lab3prog/lab3prog/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab3prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Point dot = new Point();
+                dot.Draw(0, 0, '|');
+                dot.Draw(1, 0, '+');
+                dot.Draw(2, 0, '+');
+                dot.Draw(0, 1, '|');
+                dot.Draw(3, 1, '+');
+                dot.Draw(0, 2, '|');
+                dot.Draw(1, 2, '+');
+                dot.Draw(2, 2, '+');
+                dot.Draw(0, 3, '|');
+            }
+            catch
+            {
+                Environment.Exit(0);
+            }
+            Console.ReadKey();
+        }
+    }
+}
